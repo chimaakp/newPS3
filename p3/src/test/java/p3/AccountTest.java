6a6b99a702272c4c2bd2aa4e3770b0a2d5877191
chimaakp <chimaakp@ChimaII>
2016-03-10 01:02:08 -0500

    ugh

--- p3/src/test/java/p3/AccountTest.java
+++ p3/src/test/java/p3/AccountTest.java
@@ -7,17 +7,41 @@ import base.Account;
 
 
 public class AccountTest {
-	
-	TestAccount()
-	TestAccount1()
-	TestgetMonthlyInterestRate()
-	Testgetid()
-	TestgetannualInterestRate()
-	TestgetdateCreated()
-	Testwithdraw()
-	Testdeposit
-
 	@Test
+	public void TestAccount() {
+		Account t = new Account();
+		
+		
+	}
+	
+	public void TestAccount1(){
+		
+	}
+	
+	public void TestgetMonthlyInterestRate(){
+		
+	}	
+	
+	public void Testgetid(){
+		
+	}	
+	
+	public void TestgetannualInterestRate(){
+		
+	}	
+	
+	public void TestgetdateCreated(){
+		
+	}	
+			
+	public void Testwithdraw(){
+				
+	}
+	
+	public void Testdeposit(){
+		
+	}
+
 	
 	
 		fail("Not yet implemented");
