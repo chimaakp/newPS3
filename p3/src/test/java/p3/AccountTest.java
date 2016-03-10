c94c216b9895c20b499b9c2a313d78695a0e3936
chimaakp <chimaakp@ChimaII>
2016-03-10 01:55:31 -0500

    ugh

--- p3/src/test/java/p3/AccountTest.java
+++ p3/src/test/java/p3/AccountTest.java
@@ -9,22 +9,22 @@ public class AccountTest {
 	@Test
 	public void TestAccount() {
 		Account t = new Account();
-		
-		
-	}
+		double texpected = t.getbalance();
+		double tactual = 0;
+		assertTrue(texpected == tactual);
+		}
 	
 		
 	
-	
+	@Test
 	public void TestgetMonthlyInterestRate(){
 		Account t2 = new Account(1122, 20000, 4.5);
 		double t2expected = t2.getMonthlyInterestRate();
 		double t2actual = (4.5 / 12);
 		assertTrue(t2expected == t2actual);
-		
-		
+			
 	}	
-	
+	@Test
 	public void Testgetid(){
 		Account t3 = new Account(1122, 20000, 4.5);
 		int t3expected = t3.getid();
@@ -32,28 +32,37 @@ public class AccountTest {
 		assertTrue(t3expected == t3actual);
 		
 	}	
-	
+	@Test
 	public void TestgetannualInterestRate(){
 		Account t4 = new Account(1122, 20000, 4.5);
-		int t4expected = t4.getannualInterestRate();
-		int t4actual = 4.5;
+		double t4expected = t4.getannualInterestRate();
+		double t4actual = 4.5;
 		assertTrue(t4expected == t4actual);
 		
 	}	
-	
+	@Test
 	public void TestgetdateCreated(){
 		Account t5 = new Account();
 		Date t5expected = t5.getdateCreated();
 		Date t5actual = new Date();
+		assertTrue(t5expected == t5actual);
 		
 	}	
-			
+	@Test		
 	public void Testwithdraw(){
-				
-	}
-	
-	public void Testdeposit(){
+		Account t6 = new Account(1122,20000,4.5);
+		double t6expected = t6.withdraw(2500);
+		double t6actual = 17500; 
+		assertTrue(t6expected == t6actual);
 		
+	}
+	@Test
+	public void Testdeposit(){
+		Account t7 = new Account(1122,20000,4.5);
+		double t7expected = t7.deposit(3000);
+		double t7actual = 23000; 
+		assertTrue(t7expected == t7actual);
+			
 	}
 
 	
