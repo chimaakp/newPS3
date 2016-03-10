b7811722dcbae00714b82b37b0b4623c435fbc75
chimaakp <chimaakp@ChimaII>
2016-03-10 00:48:01 -0500

    workin on tests

---
+++ p3/src/test/java/p3/AccountTest.java
@@ -0,0 +1,26 @@
+package p3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import base.Account;
+
+
+public class AccountTest {
+	
+	TestAccount()
+	TestAccount1()
+	TestgetMonthlyInterestRate()
+	Testgetid()
+	TestgetannualInterestRate()
+	TestgetdateCreated()
+	Testwithdraw()
+	Testdeposit
+
+	@Test
+	
+	
+		fail("Not yet implemented");
+	}
+
+}
